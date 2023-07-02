a4263fe6481d1ee77873fc1527c905a4bfc4dfc9
unnamed-ii <isaingashev@gmail.com>
2023-07-02 16:53:33 +0300

    started main block and created container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Main from "./blocks/Main/main";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <>
+            <Main />
+        </>
+    );
 }
 
 export default App;
---
+++ src/blocks/Main/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './main.scss';
+import Container from "../../components/Container/container";
+
+const Main = () => {
+    return (
+        <section className="main">
+            <Container>
+                <div className="main__inner">
+
+                </div>
+            </Container>
+        </section>
+    );
+};
+
+export default Main;
