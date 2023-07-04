9dc51870768e67f341f29f5bc135bfedcdaa3431
unnamed-ii <isaingashev@gmail.com>
2023-07-05 01:26:20 +0300

    updated main block

--- src/blocks/Main/main.jsx
+++ src/blocks/Main/main.jsx
@@ -7,9 +7,25 @@ const Main = () => {
     return (
         <section className="main">
             <Container>
-                <Header />
+                <Header/>
+            </Container>
+            <Container>
                 <div className="main__inner">
-
+                    <h1 className="main__inner-title">
+                        Стильный электросамокат с мощным аккумулятором
+                    </h1>
+                    <h3 className="main__inner-subtitle">
+                        Улучшенная производительность
+                        <br/>
+                        для дальних путешествий.
+                        <br/>
+                        Работает так же просто,
+                        <br/>
+                        как и выглядит.
+                    </h3>
+                    <div className="main__inner-toggle">
+                        toggle
+                    </div>
                 </div>
             </Container>
         </section>
