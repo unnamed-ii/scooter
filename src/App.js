f184aa0636b5c5e9419508728e9ae3c0051cda4a
unnamed-ii <isaingashev@gmail.com>
2023-07-23 21:24:54 +0300

    responsive for footer

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
                     {/*<Reviews/>*/}
                     <FAQ/>
                     {/*<Product/>*/}
-                    {/*<Footer/>*/}
+                    <Footer/>
                 </div>
             </SwitchButtonContext.Provider>
         </ThemeContext.Provider>
